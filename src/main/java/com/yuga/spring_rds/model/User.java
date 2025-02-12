196e93671858f192c98b61a03a395c8ed18e5c85
Chandler <r0l069h@gmail.com>
2025-02-12 19:17:33 +0530

    Changing name to username

--- src/main/java/com/yuga/spring_rds/model/User.java
+++ src/main/java/com/yuga/spring_rds/model/User.java
@@ -13,14 +13,8 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
 public class User {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
-  private String name;
-
+  private String username;
   private String email;
 }
